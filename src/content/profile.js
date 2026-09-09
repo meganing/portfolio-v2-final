ec852526c565063d28f33edbd5bc32494093296b
Thiri <shuaixue.thiri@gmail.com>
2026-09-10 00:51:18 +0700

    Remove unused portrait and compass assets

--- src/content/profile.js
+++ src/content/profile.js
@@ -9,6 +9,5 @@ export const profile = {
   email: 'shuaixue.thiri@gmail.com',
   githubUrl: 'https://github.com/meganing',
   linkedinUrl: 'https://www.linkedin.com/in/thiri-htet-thwe-9b2445357/',
-  resumeUrl: '',
-  portrait: { src: 'public/assets/painted/portrait.png', alt: 'Thiri speaking at a student event' }
+  resumeUrl: ''
 };
